2c9498e359fcec8bc24ee51628c290ba2ed3d5ba
sergenp <sergen12343@gmail.com>
2021-01-24 10:27:27 +0300

    Made so models with deleted field value 1(True) doesn't show up in api list

--- LibraryManagementSystem/libraryfrontend/views.py
+++ LibraryManagementSystem/libraryfrontend/views.py
@@ -9,25 +9,25 @@ from .models import AuthorModel, BookModel, CategoryModel, PublisherModel
 
 class AuthorView(viewsets.ReadOnlyModelViewSet):
     serializer_class = AuthorSerializer
-    queryset = AuthorModel.objects.all()
+    queryset = AuthorModel.objects.all().filter(deleted=0)
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_fields = ("name",)
 
 class BookView(viewsets.ReadOnlyModelViewSet):
     serializer_class = BookSerializer
-    queryset = BookModel.objects.all()
+    queryset = BookModel.objects.all().filter(deleted=0)
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_fields = ("name", "author", "category", "publisher")
     
 class CategoryView(viewsets.ReadOnlyModelViewSet):
     serializer_class = CategorySerializer
-    queryset = CategoryModel.objects.all()
+    queryset = CategoryModel.objects.all().filter(deleted=0)
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_fields = ("name",)
 
 class PublisherView(viewsets.ReadOnlyModelViewSet):
     serializer_class = PublisherSerializer
-    queryset = PublisherModel.objects.all()
+    queryset = PublisherModel.objects.all().filter(deleted=0)
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_fields = ("name",)
 
